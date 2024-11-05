597829d2141fbd5ea3ee8619e35fff0fd8f3b2d6
Jatin Papreja <jatinpapreja@gmail.com>
2024-11-05 13:43:43 +0530

    Created Project-2 for implementing Apache Kafka Transactions
    Moved previous code to Project-1.
    Created a new Project-2 for implementing Transactions in Apache Kafka.

---
+++ Project-1/EmailNotificationMicroservice/src/main/java/com/apachekafkamicroservices/ws/EmailNotificationMicroservice/io/ProcessedEventRepository.java
@@ -0,0 +1,10 @@
+package com.apachekafkamicroservices.ws.EmailNotificationMicroservice.io;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProcessedEventRepository extends JpaRepository<ProcessedEventEntity, Long> {
+
+	ProcessedEventEntity findByMessageId(String messageId);
+}
